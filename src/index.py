9c6f294df6807ba336b62361e12ce157f7b360bd
Joonas Tapio <joonas.tapio@helsinki.fi>
2023-04-17 14:57:48 +0300

    eriytetty logiikkaa

--- src/index.py
+++ src/index.py
@@ -1,33 +1,61 @@
 import pygame
 from taso import Taso 
-KOKO = 30
-KORKEUS = 30
-LEVEYS = 25
+KOKO = 40
+KORKEUS = 20
+LEVEYS = 14
 
 
 def main():
     pygame.init()
     pygame.display.set_caption("Tetris")
     teksti = pygame.font.Font(pygame.font.get_default_font(),18)
-     
+    oikea,vasen=False,False 
     korkeus = KORKEUS*KOKO
     leveys = LEVEYS*KOKO
+
     ruutu = pygame.display.set_mode((leveys, korkeus))
-    palaa= pygame.image.load("src/assets/nelio.png")
+    #palaa= pygame.image.load("src/assets/nelio.png")
+    palaa = pygame.surface.Surface((KOKO,KOKO))
+    palaa.fill((255,255,255))
+    pisteet=0
     x,y=0,0
-    sana = teksti.render("AAAA",True,(200,200,0),(100,100,100))
+    nelio_koko=KOKO#palaa.get_width()
+    sana = teksti.render(f"Pisteet: {pisteet}",True,(200,200,0),(100,100,100))
     # logiiikka ei vielä omassa kansiossaan
     while True:
         
-        ruutu.fill((110, 110, 110))
+        ruutu.fill((0, 0, 0))
+        ruutu.blit(sana,(leveys-sana.get_width(),0))
+        for i in range(0,11):
+            pygame.draw.lines(ruutu,(110,110,110),True,((i*nelio_koko,0),(i*nelio_koko,korkeus)))
+        for i in range(0,21):
+            pygame.draw.lines(ruutu,(110,110,110),True,((0,i*nelio_koko),(nelio_koko*10,i*nelio_koko)))
         ruutu.blit(palaa, (x, y))
-        ruutu.blit(sana,(200,200))
-        pygame.draw.line(ruutu,(0,0,0),(0,0),(100,100))
-        y+=0.1
+
+        if y <korkeus-palaa.get_height():  
+            y+=0.04
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:  
+                        vasen=True
+                    if event.key == pygame.K_RIGHT:
+                        oikea=True
+                    if event.key == pygame.K_DOWN:
+                        y=korkeus-palaa.get_height()-1
+            if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        vasen=False
+                    if event.key == pygame.K_RIGHT:
+                        oikea=False
             if event.type == pygame.QUIT:
                 return False
+        
+        if vasen and x>=0:
+            x-=1
+        if oikea and x<=9*KOKO:
+            x+=1
 
+        
         pygame.display.update()
 
 if __name__ == "__main__":
--- src/taso.py
+++ src/taso.py
@@ -1,22 +1,16 @@
 import pygame
 import random
-from sprites.j_pala import j_pala
-from sprites.l_pala import l_pala
-from sprites.nelio import nelio
-from sprites.pitkula import pitkula
-from sprites.s_pala import s_pala
-from sprites.t_pala import t_pala
-from sprites.z_pala import z_pala
+from sprites.palat import s_pala,j_pala,l_pala,n_pala,i_pala,t_pala,z_pala
 
 
 class Taso:
-    def __init__(self, korkeus, leveys):
-        self.s=s_pala(korkeus, leveys)
-        self.j=j_pala(korkeus,leveys)
-        self.l=l_pala(korkeus,leveys)
-        self.n=nelio(korkeus,leveys)
-        self.p=pitkula(korkeus,leveys)
-        self.t=t_pala(korkeus,leveys)
-        self.z=z_pala(korkeus,leveys)
+    def __init__(self, KOKO):
+        self.s=s_pala(KOKO)
+        self.j=j_pala(KOKO)
+        self.l=l_pala(KOKO)
+        self.n=n_pala(KOKO)
+        self.p=i_pala(KOKO)
+        self.t=t_pala(KOKO)
+        self.z=z_pala(KOKO)
     def pala(self):
         return random.choice([self.j,self.l,self.n,self.p,self.s,self.t,self.z])
